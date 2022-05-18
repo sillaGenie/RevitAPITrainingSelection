377a8c734fc43ed3e0570e865d5a1ab10bb2b107
Татьяна Буянова <tanyasilagina@gmail.com>
2022-05-18 15:13:49 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection/Main.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            var levels = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            string Text = null;
+            foreach (Level level in levels)
+            {
+                var ducts = new FilteredElementCollector(doc)
+                    .OfClass(typeof(Duct))
+                    .OfType<Duct>()
+                    .Where ( x=> x.get_Parameter(BuiltInParameter.RBS_START_LEVEL_PARAM).AsValueString()==level.Name)
+                    .Count();
+                Text += level.Name + ": " + ducts.ToString() + "воздуховодов" + "\n";
+            }
+            TaskDialog.Show("Количество воздуховодов по этажам", Text);
+
+            return Result.Succeeded;
+        }
+    }
+}
